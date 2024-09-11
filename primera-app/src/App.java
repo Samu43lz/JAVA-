38e67bc29d0fc53757e7656bdc55bde1e6543b61
Samuel <widosrevel@gmail.com>
2024-09-10 21:27:10 -0500

    tipos de datos primitivos

--- primera-app/src/App.java
+++ primera-app/src/App.java
@@ -1,5 +1,52 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! by Samuel from Java!");
+        
+      
+      /* System.out.println("Hello, World! by Samuel from Java!"); */
+
+      
+      // Una variable es un espacio en memoria que tiene un nombre asociado y puede contener un valor cambiante
+
+      //DECLARACIÓN: Es asignar el tipo de dato y el nombre de la variable sin darle un valor inicial
+
+      // ASIGNACIÓN: Darle un valor a una variable.
+
+      /* 
+      TIPOS DE VARIABLES
+      Variables primitivas: Almacenan valores básicos.
+      Variables de referencia: Almacenan direecciones de memoria que apuntan a objetos.
+      byte(-128 al 127), short(-32768 al 32767), int, long
+      */
+      
+      int numero = 5;
+      numero = 5;
+      byte numero1 = 20;
+
+      //TIPOS DE DATOS REALES: float (6) decimales y el double (15-16) decimales
+      double decimales = 5.11232131;
+
+      //TIPO DE DATO DE CARACTER (una solta letra)
+      char letra = 's';
+
+      //TIPO DE DATO BOOLEANO
+      boolean v = true;
+      boolean f = false;
+
+      String cadena_caracteres = "You're so fuck#ng good!";
+
+
+      System.out.println(numero);
+      System.out.println(decimales);
+      System.out.println(letra);
+      System.out.println(v);
+      System.out.println(f);
+      System.out.println(cadena_caracteres);
+
+
+
+
+
+
+
     }
 }
